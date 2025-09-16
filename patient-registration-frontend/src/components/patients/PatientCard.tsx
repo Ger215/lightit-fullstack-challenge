3f21b45172c52349dcd571b15fdd21c39557f136
Ger215 <gsrp0310@gmail.com>
2025-09-16 17:02:43 -0300

    feat: Patient Cards && Page Navigation

--- patient-registration-frontend/src/components/patients/PatientCard.tsx
+++ patient-registration-frontend/src/components/patients/PatientCard.tsx
@@ -1,52 +1,65 @@
 import { useState } from 'react';
-
-interface Patient {
-  id: string;
-  fullName: string;
-  email: string;
-  countryCode: string;
-  phoneNumber: string;
-  documentPhoto: string;
-  createdAt: string;
-}
+import type { Patient } from '../../domain/Patient';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
 
 export function PatientCard({ patient }: { patient: Patient }) {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className="border rounded-lg shadow-sm p-4 bg-white">
+    <div
+      className="bg-white rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 p-5 cursor-pointer"
+      onClick={() => setExpanded((prev) => !prev)}
+    >
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
           <img
             src={`http://localhost:3000/uploads/${patient.documentPhoto}`}
             alt="Document"
-            className="w-16 h-16 rounded object-cover border"
+            className="w-16 h-16 rounded-full object-cover border-2 border-blue-200 shadow-sm"
           />
-          <h2 className="font-bold text-lg">{patient.fullName}</h2>
+          <h2 className="font-semibold text-xl text-gray-800">
+            {patient.fullName}
+          </h2>
         </div>
         <button
-          onClick={() => setExpanded((prev) => !prev)}
-          className="text-blue-600 hover:underline"
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            setExpanded((prev) => !prev);
+          }}
+          className="cursor-pointer text-blue-600 hover:text-blue-800 transition-colors duration-500 flex items-center gap-1"
         >
-          {expanded ? 'Hide' : 'Expand'}
+          {expanded ? (
+            <>
+              <ChevronUpIcon className="w-5 h-5" />
+            </>
+          ) : (
+            <>
+              <ChevronDownIcon className="w-5 h-5" />
+            </>
+          )}
         </button>
       </div>
 
-      {expanded && (
-        <div className="mt-4 text-sm text-gray-700">
+      <div
+        className={`overflow-hidden transition-all duration-500 ease-in-out ${
+          expanded ? 'max-h-40 mt-4 opacity-100' : 'max-h-0 opacity-0'
+        }`}
+      >
+        <div className="text-md text-gray-700 space-y-2">
           <p>
             <span className="font-semibold">Email:</span> {patient.email}
           </p>
           <p>
-            <span className="font-semibold">Phone:</span> {patient.countryCode}{' '}
-            {patient.phoneNumber}
+            <span className="font-semibold">Phone:</span> ({patient.countryCode}
+            ) {patient.phoneNumber}
           </p>
           <p>
-            <span className="font-semibold">Created At:</span>{' '}
+            <span className="font-semibold">Registration Date:</span>{' '}
             {new Date(patient.createdAt).toLocaleDateString()}
           </p>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- patient-registration-frontend/src/pages/PatientsPage.tsx
+++ patient-registration-frontend/src/pages/PatientsPage.tsx
@@ -2,10 +2,20 @@ import { useEffect, useState } from 'react';
 import { PatientCard } from '../components/patients/PatientCard';
 import type { Patient } from '../domain/Patient';
 import { getPatients } from '../services/patientsService';
+import {
+  paginate,
+  goToNextPage,
+  goToPreviousPage,
+} from '../services/paginationService';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 
 export function PatientsPage() {
   const [patients, setPatients] = useState<Patient[]>([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const patientsPerPage = 5;
 
   useEffect(() => {
     const fetchPatients = async () => {
@@ -22,19 +32,34 @@ export function PatientsPage() {
     fetchPatients();
   }, []);
 
+  const { paginatedData: paginatedPatients, totalPages } = paginate(
+    patients,
+    currentPage,
+    patientsPerPage
+  );
+
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-gray-600 text-lg">Loading patients...</p>
+      <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-blue-50 to-blue-100">
+        <div className="w-16 h-16 border-4 border-blue-300 border-t-transparent rounded-full animate-spin"></div>
+        <p className="text-gray-600 text-lg mt-4 font-medium">
+          Loading Patients...
+        </p>
       </div>
     );
   }
 
   if (!loading && patients.length === 0) {
     return (
-      <div className="flex flex-col justify-center items-center h-screen">
-        <p className="text-gray-600 text-lg mb-4">No patients found</p>
-        <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+      <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-blue-50 to-blue-100 text-center">
+        <ExclamationCircleIcon className="h-16 w-16 text-gray-400 mb-4" />
+        <p className="text-gray-700 text-xl font-semibold mb-2">
+          No patients found
+        </p>
+        <p className="text-gray-500 mb-6">
+          Add your first patient to get started.
+        </p>
+        <button className="bg-blue-600 text-white px-6 py-3 rounded-lg font-medium shadow hover:bg-blue-700 transition">
           + Add Patient
         </button>
       </div>
@@ -42,18 +67,52 @@ export function PatientsPage() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto py-10 px-4">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">Patients</h1>
-        <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-          + Add Patient
-        </button>
-      </div>
+    <div className="min-h-screen w-full bg-gradient-to-br from-blue-50 to-blue-100">
+      <div className="w-full max-w-4xl mx-auto px-4 pt-10">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold">Patients</h1>
+          <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+            + Add Patient
+          </button>
+        </div>
 
-      <div className="grid gap-4">
-        {patients.map((p) => (
-          <PatientCard key={p.id} patient={p} />
-        ))}
+        <div
+          className={`grid gap-4 ${
+            patients.length >= 4
+              ? 'max-h-[500px] overflow-y-auto pr-2 rounded-lg'
+              : ''
+          }`}
+        >
+          {paginatedPatients.map((p) => (
+            <PatientCard key={p.id} patient={p} />
+          ))}
+        </div>
+
+        {totalPages > 1 && (
+          <div className="flex justify-center items-center gap-6 mt-8">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage((prev) => goToPreviousPage(prev))}
+              className="cursor-pointer p-2 rounded-full bg-blue-400 text-white shadow hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed transition"
+            >
+              <ChevronLeftIcon className="w-6 h-6" />
+            </button>
+
+            <span className="text-gray-700 font-semibold">
+              Page {currentPage} of {totalPages}
+            </span>
+
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() =>
+                setCurrentPage((prev) => goToNextPage(prev, totalPages))
+              }
+              className="cursor-pointer p-2 rounded-full bg-blue-400 text-white shadow hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed transition"
+            >
+              <ChevronRightIcon className="w-6 h-6" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
